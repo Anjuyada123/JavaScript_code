d2443749c8538a857011429646f295b989b9face
ANJU YADAV <120616137+Anjuyada123@users.noreply.github.com>
2026-02-24 19:02:44 +0000

    conversion

--- basics/datatypes.js
+++ basics/datatypes.js
@@ -17,3 +17,21 @@ let isLoggedIn=false;
 console.log(typeof(2));
 console.log(typeof(null)); //type-object
 console.log(typeof(undefined));
+
+//convert string to number
+
+let score="33";
+console.log(typeof(score));
+let valueIntNumber=Number(score);
+console.log(typeof(valueIntNumber));
+console.log(valueIntNumber);
+
+//33ac will convert in number but in NAN
+//true convert in 1,false-0
+
+let isLoggedInn=1;
+let booleanIsLoggedInn=Boolean(isLoggedInn);
+console.log(booleanIsLoggedInn);
+
+//string-1
+//empty string-false
