69d698db84a0d9936b80d1b1f633368cee08595c
ANJU YADAV <120616137+Anjuyada123@users.noreply.github.com>
2026-02-24 19:19:19 +0000

    prefix

---
+++ basics/operations.js
@@ -0,0 +1,14 @@
+let value=3
+let negValue=-value; //negative of value
+console.log(negValue);
+
+//console(string+number+number)=string
+//console(number+number+string)=number+string
+
+console.log(+true);
+console.log(+"");  //precedence
+
+//prefix and postfix 
+let a = 10;
+console.log(a++); // 10
+console.log(++a); // 12
